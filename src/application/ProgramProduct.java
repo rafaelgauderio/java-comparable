b47d33caeb0bd895bca275b96adf1dc362993865
Rafael de Luca <deluca1712@gmail.com>
2023-06-24 19:27:57 -0300

    Comparable Product order by name ascending

---
+++ src/application/ProgramProduct.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import entidades.Product;
+
+public class ProgramProduct {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		List<Product> productList = new ArrayList<Product> ();
+		Product p1 = new Product("Chair", 150.90);
+		Product p2 = new Product("Table", 850.50);
+		Product p3 = new Product("Box bed", 1250.90);
+		Product p4 = new Product("Cabinet", 320.20);
+		Product p5 = new Product("Armchair", 1200.60);
+		Product p6 = new Product("Marble sink", 1600.00);
+		
+		productList.add(p1);
+		productList.add(p2);
+		productList.add(p3);
+		productList.add(p4);
+		productList.add(p5);
+		productList.add(p6);
+		
+		// order by name ascending
+		Collections.sort(productList); 
+		
+		for(Product nickname : productList) {
+			System.out.println(nickname);
+		}
+		
+		
+
+	}
+
+}
